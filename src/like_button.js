5bde1d4ce9c0f1bce7bc2349cc896808145608cd
pigongting <466548808@qq.com>
2019-02-27 23:19:05 +0800

    test

--- src/like_button.js
+++ src/like_button.js
@@ -13,7 +13,7 @@ class LikeButton extends React.Component {
 
     return (
       <button onClick={() => this.setState({ liked: true })}>
-        Like23
+        Like234
       </button>
     );
   }
